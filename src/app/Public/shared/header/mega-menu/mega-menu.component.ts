a54a93aebe80e17e00c206bb8c3695dce2ae06c4
assmabaziz <assma.baziz@yahoo.com>
2025-06-25 18:43:33 +0300

    logic to toggle menu added

--- src/app/Public/shared/header/mega-menu/mega-menu.component.ts
+++ src/app/Public/shared/header/mega-menu/mega-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-mega-menu',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './mega-menu.component.scss'
 })
 export class MegaMenuComponent {
-
+@Input({required: true}) showMenu = false
 }
