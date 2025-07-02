2f35fda6f48e16efc35754f0bd6fc45c7c5da4b5
assmabaziz <assma.baziz@yahoo.com>
2025-07-02 17:11:39 +0300

    design best selling section

--- src/app/Public/home/home.component.ts
+++ src/app/Public/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "./banner/banner.component";
+import { BestSellerComponent } from "./best-seller/best-seller.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BannerComponent],
+  imports: [BannerComponent, BestSellerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
