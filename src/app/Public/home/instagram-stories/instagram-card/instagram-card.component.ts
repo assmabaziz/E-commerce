f43474767b659fda46e70f683dbba6ad70056156
assmabaziz <assma.baziz@yahoo.com>
2025-07-24 13:19:30 +0300

    insagram card component created

---
+++ src/app/Public/home/instagram-stories/instagram-card/instagram-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instagram-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './instagram-card.component.html',
+  styleUrl: './instagram-card.component.scss'
+})
+export class InstagramCardComponent {
+
+}
