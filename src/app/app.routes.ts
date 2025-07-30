efe478db3f985fd6ae620d9e82951a9b72cf580e
assmabaziz <assma.baziz@yahoo.com>
2025-07-30 16:03:35 +0300

    auth components created, routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,18 @@ export const routes: Routes = [
         loadChildren: ()=> import('./Public/public.module').then(m=>m.PublicModule)
     },
     {
+        path:'login',
+        loadComponent:()=> import ('./Acount/login/login.component').then(c=>c.LoginComponent)
+    },
+    {
+        path:'register',
+        loadComponent:()=> import ('./Acount/register/register.component').then(c=>c.RegisterComponent)
+    },
+    {
+        path:'forgot-password',
+        loadComponent:()=> import ('./Acount/forgot-password/forgot-password.component').then(c=>c.ForgotPasswordComponent)
+    },
+    {
         path:'',
         pathMatch:'full',
         redirectTo:'/public'
