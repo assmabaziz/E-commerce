2cdc693ee8062e55b227f3940ec24214e5be437e
assmabaziz <assma.baziz@yahoo.com>
2025-07-31 16:50:04 +0300

    start working on login form and logic

--- src/app/Acount/login/login.component.ts
+++ src/app/Acount/login/login.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { MaterialModule } from '../../Shared/Modules/material.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [MaterialModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  hide: boolean = false;
+  constructor(private formbuilder: FormBuilder, private router: Router) {}
+  ngOnInit(): void {
+    this.initFormModels();
+  }
+  signIn(): void {
+    console.log(this.loginForm.value);
+  }
+  navigateToForgetPasswordPage(): void {
+    this.router.navigateByUrl('forgot-password');
+  }
+  private initFormModels(): void {
+    this.loginForm = this.formbuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      rememberMe: [false],
+    });
+  }
 }
--- src/app/Shared/Modules/material.module.ts
+++ src/app/Shared/Modules/material.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPseudoCheckboxModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 const modules = [
    CommonModule,
-    MatInputModule 
+    MatInputModule,
+    MatFormFieldModule,
+    MatPseudoCheckboxModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatIconModule,
+    MatIconModule,
+    MatCheckboxModule
 ]
 @NgModule({
   declarations: [],
